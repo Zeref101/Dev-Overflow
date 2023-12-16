422b8357f2bd188f2babafffc8eafd085febd108
Shreyas <shreyas.mohanty03@gmail.com>
2023-12-16 11:36:16 +0530

    auth and layout updated

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            Ask Questions
+        </div>
+    )
+}
+
+export default page
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <div className="h1-bold">Guten Tag!</div>
-  )
-}
