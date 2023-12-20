097d468d083d1d2e1ff62dc8776643156034af40
Shreyas <shreyas.mohanty03@gmail.com>
2023-12-20 16:15:55 +0530

    shared tag component added

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { Badge } from '../ui/badge'
 
 interface Props {
-    _id: number,
-    name: string,
-    totalQuestions?: number,
-    showCount?: number
+    _id: string;
+    name: string;
+    totalQuestions?: number;
+    showCount?: boolean;
 }
 
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
