3097f858279174b95cd8e17c8f52416b9247bca9
Shreyas <shreyas.mohanty03@gmail.com>
2023-12-21 11:41:16 +0530

    validation for ask question made

---
+++ lib/validations.ts
@@ -0,0 +1,26 @@
+import * as z from "zod";
+
+export const QuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Please enter a title with at least 5 characters." })
+    .max(130, {
+      message: "Limit your title to a maximum of 130 characters.",
+    }),
+  explaination: z.string().min(100, {
+    message:
+      "Please provide a detailed explanation with a minimum of 100 characters.",
+  }),
+  tags: z
+    .array(
+      z.string().min(1).max(5, {
+        message: "Each tag should have between 1 and 5 characters.",
+      })
+    )
+    .min(1, {
+      message: "Include at least one tag.",
+    })
+    .max(3, {
+      message: "You can add up to a maximum of 3 tags.",
+    }),
+});
