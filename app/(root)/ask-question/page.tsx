b8f11664d61d106dbb858cd156ceaa1d3eb6d108
Shreyas <shreyas.mohanty03@gmail.com>
2023-12-29 13:47:35 +0530

    Mocking dummy user to test

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,20 @@
 import { Question } from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  // const { userId } = auth();
+  const userId = "CL123456";
+  if (!userId) redirect('/sign-in');
+  const mongoUser = await getUserById({ userId });
+  // console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark200_light900">Ask a question</h1>
       <div>
-        <Question />
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   );
