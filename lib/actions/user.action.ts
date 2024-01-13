5e8c6eba0c28329a53677ad5c5f807dc27d7c689
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-14 02:08:35 +0530

    updated

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,9 +63,9 @@ export async function deleteUser(params: DeleteUserParams) {
 
     //  get questions of that particular user
 
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id",
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id",
+    // );
 
     //  delete user questions
 
