609194171d143238e50ded66019ad622b3bf8d18
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-16 20:53:51 +0530

    webhook url updated

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -9,11 +9,11 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
 
   //   TODO: ADD WEBHOOK SECRET TO .env.local
-  const WEBHOOK_SECRET = process.env.NEXT_CLERK_SECRET_WEBHOOK;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
