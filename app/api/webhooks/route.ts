fd7a72b32ea2e28877d8b2cdd0c2c38f0a33e481
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-21 10:11:21 +0530

    removed clg

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -57,9 +57,6 @@ export async function POST(req: Request) {
   // const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(eventType);
-  console.log(evt.data);
-
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
