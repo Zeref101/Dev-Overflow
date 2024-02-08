9bea3281c3f134dafcceee157d3ab8c452e693f7
Shreyas <shreyas.mohanty03@gmail.com>
2024-02-08 13:48:51 +0530

    get all tags action initial stage

--- lib/actions/tag.action.tsx
+++ lib/actions/tag.action.tsx
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectionToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function gettopInteractedTags(params: GetTopInteractedTagsParams) {
     try {
@@ -16,6 +17,18 @@ export async function gettopInteractedTags(params: GetTopInteractedTagsParams) {
         //  find interaction for the user and group by tags
 
         return [{ _id: 1, name: 'tag1' }, { _id: 2, name: 'tag2' }];
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+    try {
+        connectionToDatabase();
+        const tags = await Tag.find({});
+        return { tags }
+
     } catch (error) {
         console.log(error);
         throw error;
