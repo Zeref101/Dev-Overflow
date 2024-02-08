fe53ac16ec5e4ff32ff68a6c0d2d5d6c5c486293
Shreyas <shreyas.mohanty03@gmail.com>
2024-02-08 17:23:26 +0530

    designed tag card

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -3,6 +3,7 @@ import NoResult from '@/components/shared/NoResult'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
 import { TagFilters } from '@/constants/filter'
 import { getAllTags } from '@/lib/actions/tag.action'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = async () => {
@@ -31,9 +32,26 @@ const Page = async () => {
                     result.tags.map((tag) => {
                         console.log(tag);
                         return (
-                            <div key={tag._id}>
-                                Tag Card
-                            </div>
+                            <Link
+                                href={`/tags/${tag._id}`}
+                                key={tag._id}
+                                className=' shadow-light100_darknone '
+                            >
+                                <article className='background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]'>
+                                    <div className=' background-light800_dark400 w-fit rounded-sm px-5 py-1.5'>
+                                        <p className=' paragraph-semibold text-dark300_light900'>
+                                            {tag.name}
+                                        </p>
+                                    </div>
+                                    <p className=' small-medium text-dark400_light500 mt-3.5'>
+                                        <span className=' body-semibold primary-text-gradient mr-2.5'>
+                                            {tag.Questions.length}+
+                                        </span>
+                                        Questions
+                                    </p>
+                                </article>
+
+                            </Link>
 
                         )
                     })
