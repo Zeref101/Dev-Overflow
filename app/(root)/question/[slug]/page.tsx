a2cf4ecfe289c94d69c1570e25e3945bd478284d
Shreyas <shreyas.mohanty03@gmail.com>
2024-02-10 16:18:13 +0530

    question content now parsed and tags are visible

--- app/(root)/question/[slug]/page.tsx
+++ app/(root)/question/[slug]/page.tsx
@@ -1,19 +1,25 @@
+import Answer from '@/components/forms/Answer';
 import Metric from '@/components/shared/Metric';
 import ParseHTML from '@/components/shared/ParseHTML';
+import RenderTag from '@/components/shared/RenderTag';
 import { getQuestionById } from '@/lib/actions/question.action'
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
+interface QuestionTagProps {
+    _id: string;
+    name: string;
+}
 
 const Page = async ({ params }: { params: { slug: string } }) => {
     const questionId = params.slug;
     const result = await getQuestionById({ questionId });
-    console.log(result.question);
+    // console.log(result.question);
     return (
         <>
-            <div className=' flex-start w-full flex-col'>
+            <div className=' flex w-full flex-col items-start justify-start'>
                 <div className=' flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
                     <Link
                         href={`/profile/${result.question.author.clerk_id}`}
@@ -35,7 +41,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
                         VOTING
                     </div>
                 </div>
-                <div className=' flex flex-col'>
+                <div className=' flex flex-col justify-start'>
                     <h2 className=' h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>
                         {result.question.title}
                     </h2>
@@ -44,7 +50,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
                             imgUrl="/assets/icons/clock.svg"
                             alt="clockIcon"
                             value={`asked ${getTimestamp(result.question.createdAt)}`}
-                            title="Asked"
+                            title=""
                             textStyles="small-medium text-dark400_light800"
                         />
                         <Metric
@@ -62,11 +68,32 @@ const Page = async ({ params }: { params: { slug: string } }) => {
                             textStyles="small-medium text-dark400_light800"
                         />
                     </div>
-                    <ParseHTML />
 
                 </div>
 
             </div>
+            <div className=' text-dark200_light900'>
+
+                <ParseHTML
+                    data={result.question.content}
+                />
+            </div>
+            <div className=' mt-8 flex gap-4'>
+
+                {result.question.tag.map((tag: QuestionTagProps) => {
+
+                    return (
+                        <RenderTag
+                            key={tag._id}
+                            _id={tag._id}
+                            name={tag.name}
+                            showCount={false}
+
+                        />
+                    )
+                })}
+            </div>
+            <Answer />
         </>
     )
 }
