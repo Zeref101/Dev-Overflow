7387ee3c8dfd953638e67b3a09e40e9678320eec
Shreyas <shreyas.mohanty03@gmail.com>
2024-02-20 15:55:26 +0530

    Votes for question added

--- app/(root)/question/[slug]/page.tsx
+++ app/(root)/question/[slug]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
@@ -48,7 +49,18 @@ const Page = async ({ params }: { params: { slug: string } }) => {
               {result.question.author.name}
             </p>
           </Link>
-          <div className="flex justify-end text-white">VOTING</div>
+          <div className="flex justify-end text-white">
+            <Votes
+              type={"question"}
+              itemId={JSON.stringify(result.question._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.question.upvotes.length}
+              hasupVoted={result.question.upvotes.includes(mongoUser._id)}
+              downvotes={result.question.downvotes.length}
+              hasdownVoted={result.question.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result.question._id)}
+            />
+          </div>
         </div>
         <div className=" flex flex-col justify-start">
           <h2 className=" h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
