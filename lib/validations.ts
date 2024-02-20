31c299891431eb62e9fcaffee6b79a3448e4c58a
Shreyas <shreyas.mohanty03@gmail.com>
2024-02-20 09:25:57 +0530

    validation for answer schema

--- lib/validations.ts
+++ lib/validations.ts
@@ -15,7 +15,7 @@ export const QuestionSchema = z.object({
     .array(
       z.string().min(1).max(15, {
         message: "Each tag should have between 1 and 5 characters.",
-      }),
+      })
     )
     .min(1, {
       message: "Include at least one tag.",
@@ -23,4 +23,8 @@ export const QuestionSchema = z.object({
     .max(3, {
       message: "You can add up to a maximum of 3 tags.",
     }),
+});
+
+export const AnswerSchema = z.object({
+  answer: z.string().min(100),
 });
